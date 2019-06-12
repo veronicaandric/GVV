1159e79a8d9a0e434c4906280fd3794a0bb408c1
umayat <umayat@avenuecode.com>
2019-06-12 00:22:52 -0400

    Moved AppHelper closer to root

--- src/AppHelper.js
+++ src/AppHelper.js
@@ -4,13 +4,13 @@ class AppHelper {
     this.height = window.innerHeight;
     this.orientation = 'portrait';
     this.deviceType = 'laptop';
-    this.ip = '192.168.0.27';
+    this.ip = '';
     this.ports = {
       app: '8080',
       socket: '3000',
       tuio: '5000',
     };
-    this.data = $.getJSON( "../testing/Sample_Rare05_Dmg_RESULT_Copy.json");
+    this.data = $.getJSON( "./testing/Sample_Rare05_Dmg_RESULT_Copy.json");
   }
 
   getWindowSize() {
